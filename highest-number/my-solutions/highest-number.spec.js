3981867aa973b30aad56ec1da7d49e6cdfbe79e7
hector.arias.nieto@gmail.com <#P3licula>
2020-11-27 17:55:49 +0100

    solved

--- highest-number/my-solutions/highest-number.spec.js
+++ highest-number/my-solutions/highest-number.spec.js
@@ -2,10 +2,10 @@ import { whatsHighest } from './highest-number.js'
 
 describe('highest', () => {
   it('should get the highest number', () => {
-    const given = [1,2,3]
+    const given = [1, 2, 3, 4, 345]
 
     const actual = whatsHighest(given)
 
-    expect(actual).toBe(3)
+    expect(actual).toBe(345)
   })
 })
