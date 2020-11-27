63fae5956feffde7736953ef6d0ae9b3ad682132
hector.arias.nieto@gmail.com <#P3licula>
2020-11-27 17:15:30 +0100

    get five!

---
+++ highest-number/my-solutions/highest-number.spec.js
@@ -0,0 +1,11 @@
+import { whatsHighest } from './highest-number.js'
+
+describe('highest', () => {
+  it('should get the highest number', () => {
+    const given = list
+
+    const actual = whatsHigest(given)
+
+    expect(actual).toBe(5)
+  })
+})
