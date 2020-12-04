1aefa84ba026aca22eea2ae843e0f1423687ac5f
hector.arias.nieto@gmail.com <#P3licula>
2020-12-04 16:48:21 +0100

    first try pwr**2

---
+++ power-of-two/my-solutions/power-of-two.js
@@ -0,0 +1,10 @@
+export function getPoweredByTwo(){
+  const numbers = [1, 2, 3]
+  const power = []
+
+  for (let i = 1; i < numbers.length; i++){
+    power.push = numbers[i] ** 2
+    return power
+  }
+
+}
