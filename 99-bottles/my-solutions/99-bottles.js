45d83a3329400f09cc6cd8d7e3a153a58e6aad6c
hector.arias.nieto@gmail.com <#P3licula>
2020-12-04 21:45:16 +0100

    saving friday

--- 99-bottles/my-solutions/99-bottles.js
+++ 99-bottles/my-solutions/99-bottles.js
@@ -17,13 +17,8 @@ export class Sing {
   }
 
   anyVerse(n){
-    return n +
-      ' bottles of beer on the wall, ' +
-      n +
-      ' bottles of beer.\n' +
-      'Take one down and pass it around, ' +
-      (n - 1) +
-      'bottles of beer on the wall.'
+    return `${n} bottles of beer on the wall, ${n} bottles of beer.\n' +
+      'Take one down and pass it around, ${n - 1} bottles of beer on the wall.`
   }
 }
   entireSong(){
--- 99-bottles/my-solutions/99-bottles.spec.js
+++ 99-bottles/my-solutions/99-bottles.spec.js
@@ -2,8 +2,9 @@ import { Sing } from './99-bottles'
 
 describe('Sing', fn () => {
   it ('Should sing the entire song 99 Bottles', fn () =>{
-
     const canta = new Sing()
+    const given = 'No more bottles of beer on the wall, no more bottles of beer.\n' +
+      'Go to the store and buy some more, 99 bottles of beer on the wall.'
 
     const actual = canta.lastVerse()
 
